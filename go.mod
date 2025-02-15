534622efc8bfa8ebb8d3ba88ace2340a30d4b3eb
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2025-02-15 18:48:30 -0500

    added gosseract

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/edubank
 
 go 1.23.3
+
+require github.com/otiai10/gosseract/v2 v2.4.1
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/otiai10/gosseract/v2"
+)
 
 func main() {
-	fmt.Println("Start backend here")
+	client := gosseract.NewClient()
+	defer client.Close()
+
+	client.SetImage("image.png")
+
+	text, err := client.Text()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(text)
 }
