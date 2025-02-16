3b889866abf69d042c85740efa44e066766a8fbc
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2025-02-15 19:14:24 -0500

    gcp vision api implementation tested

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/edubank
 
 go 1.23.3
-
-require github.com/otiai10/gosseract/v2 v2.4.1
--- main.go
+++ main.go
@@ -1,22 +1,55 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-
-	"github.com/otiai10/gosseract/v2"
+	"net/http"
+	"os"
 )
 
+const apiKey = "YOUR_GOOGLE_CLOUD_VISION_API_KEY" // Replace with your API Key
+
 func main() {
-	client := gosseract.NewClient()
-	defer client.Close()
-
-	client.SetImage("image.png")
-
-	text, err := client.Text()
+	imagePath := "image.jpg"
+	imageData, err := os.ReadFile(imagePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to read image: %v", err)
 	}
 
-	fmt.Println(text)
+	encodedImage := base64.StdEncoding.EncodeToString(imageData)
+
+	requestBody, err := json.Marshal(map[string]interface{}{
+		"requests": []map[string]interface{}{
+			{
+				"image": map[string]string{
+					"content": encodedImage,
+				},
+				"features": []map[string]string{
+					{"type": "TEXT_DETECTION"},
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Failed to create JSON request: %v", err)
+	}
+
+	url := fmt.Sprintf("https://vision.googleapis.com/v1/images:annotate?key=%s", apiKey)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatalf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response: %v", err)
+	}
+
+	fmt.Println("OCR Response:")
+	fmt.Println(string(body))
 }
